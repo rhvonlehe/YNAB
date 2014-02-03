59c9e30ea058646efac371217c7f2c82ef6bd523
Rich von Lehe <rhvonlehe@gmail.com>
2014-02-02 21:52:14 -0600

    Initial working version of Fidelity csv to YNAB csv converter.

---
+++ fidelity-to-ynab.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+
+"""
+Convert a fidelity csv file to one accepted by YNAB
+"""
+
+import csv
+import sys
+import re
+
+def isDateRecord(record):
+	# Use regex to retrieve date field
+	# Tricky part is to cut off immediately after the year '$', otherwise you
+	# get a record with a "Downloaded on xx/yy/zzzz hh:mm pm"
+
+	match = re.search(r'\d{2}/\d{2}/\d{4}$', record[0])
+	return (match)
+
+def convertRecord(record):
+	# Reduce unwanted fields.  Tricky part: if the transaction amount field
+	# is negative (outflow), leave it in the 2nd to last position.  If 
+	# it's positive, move it to the last position (inflow)
+	#
+	newRecord = record[0:2]
+	newRecord += ['','']
+	if (float(record[10]) < 0):
+		print 'processing negative'
+		newRecord += [str(abs(float(record[10]))), '']
+	else:
+		print 'processing positive'
+		newRecord += ['', record[10]]
+	print newRecord
+	return newRecord
+
+def convertCsvFile(argv):
+	# Goal of this program is to read in line-by-line a csv file in the 
+	# fidelity-standard format. Convert it to the expected YNAB format:
+	# Date,Payee,Category,Memo,Outflow,Inflow
+	# 07/25/10,Sample Payee,,Sample Memo for an outflow,100.00,
+	# 07/26/10,Sample Payee 2,,Sample memo for an inflow,,500.00
+	#
+	# Method: open the file using a cvsreader, massage it, write it back using
+	# a cvswriter.  How do you skip over empty lines and text lines and only
+	# convert those that have meaningful transactions?  
+	#
+	with open(argv[0]) as fidelityFile:
+		with open('out.csv', 'wb') as ynabFile:
+			fidelityReader = csv.reader(fidelityFile)	
+			ynabWriter = csv.writer(ynabFile)
+			ynabWriter.writerow(['Date','Payee','Category','Memo','Outflow',
+				'Inflow'])
+			for record in fidelityReader:
+				# Is the record a transaction?  Parse it for a recognizable date.
+				#
+				if (record):
+					if (isDateRecord(record)):
+						# Found record with date, no convert it and add to
+						# outgoing data
+						#
+						newRecord = convertRecord(record)
+						ynabWriter.writerow(newRecord)
+
+
+
+
+if __name__ == "__main__":
+	convertCsvFile(sys.argv[1:])
